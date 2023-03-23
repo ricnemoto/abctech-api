052bfcb3dd5b4e463e8399aa90f659b39fa5eb81
Ricardo Nemoto <ricnemoto@gmail.com>
2023-03-23 20:55:31 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -0,0 +1,33 @@
+package br.com.fiap.abctechapi.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.springframework.beans.propertyeditors.InputStreamEditor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+    
+
+    @GetMapping
+    public ResponseEntity<String> status() {
+        return ResponseEntity.ok("Up");
+    }
+
+    @GetMapping("/version")
+    public ResponseEntity<String> version() throws IOException {
+        Properties properties = new Properties();
+
+        InputStream is = getClass().getClassLoader().getResourceAsStream("application.yml");
+
+        properties.load(is);
+
+        return ResponseEntity.ok(properties.getProperty("name") + " - "  + properties.getProperty("version"));
+    }
+}
